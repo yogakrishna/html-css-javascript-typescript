19010bcce15f5d3f719db233a00e85a2eaea5507
yogakrishna <56419760+yogakrishna@users.noreply.github.com>
2020-05-30 21:05:43 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,6 @@
+
+
+document.write("<h1> this is my heading</h1>");
+document.write("<h2> this is my heading</h2>");
+document.write("<h3> this is my heading</h3>"); 
+document.write("<h4> this is my heading</h4>");
---
+++ script.js
@@ -0,0 +1,66 @@
+/* ***************
+  
+  variables and datatypes
+
+*/
+/*
+var firstName = 'yoga';
+console.log(firstName);
+
+var lastName = 'krishna';
+var age = 21;
+console.log(age);
+
+var fullAge =  true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job='Teacher';
+console.log(job);
+*/
+
+
+
+
+var firstName = 'john';
+var age = 28;
+
+console.log(firstName+ ' ' + age);
+
+var job, isMarried;
+job = 'teacher'
+isMarried = false;
+
+console.log(firstName + 'is a '+ age + ' year old '+ job+'. Is he Married? '+ isMarried);
+
+// variable mutation
+ age = 'twenty eight';
+ job = 'driver';
+
+
+alert(firstName + 'is a '+ age + ' year old '+ job+'. Is he Married? '+ isMarried);
+
+
+var lastName = prompt('what is his last name');
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
